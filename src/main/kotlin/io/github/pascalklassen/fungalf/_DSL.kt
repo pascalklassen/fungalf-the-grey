56881481ae57910beb2352883fd66d1321e5f9a5
Pascal Klaßen <p.klassen221199@gmail.com>
2021-08-15 22:54:20 +0200

    Finalize layout concept

--- src/main/kotlin/io/github/pascalklassen/fungalf/_DSL.kt
+++ src/main/kotlin/io/github/pascalklassen/fungalf/_DSL.kt
@@ -3,4 +3,81 @@ package io.github.pascalklassen.fungalf
 @DslMarker
 annotation class MessageDsl
 
+/*val message = message {
+    embed {
+        url = "https://someurl.de"
+        title = "this is a test"
+        description = "some description"
+        thumbnail = "https://someurl.de"
+        provider {
+            name = "Dict.cc"
+            url = "https://dict.cc"
+        }
+        author {
+            name = "Pascal"
+            url = "https://someurl.de"
+        }
+        image = "https://someurl.de"
+        footer {
+            text = "Some text"
+            url = "https://someurl.de"
+        }
+    }
 
+    buttonPrimary<Label> {
+        id("someId")
+        label("My Label")
+    }
+    buttonPrimary<Emoji> {
+        id("someId")
+        emoji(Emoji.from("test"))
+    }
+    buttonSecondary<Label> {
+        id("someId")
+        label("My Label")
+    }
+    buttonSecondary<Emoji> {
+        id("someId")
+        emoji(Emoji.from("test"))
+    }
+    buttonSuccess<Label> {
+        id("someId")
+        label("My Label")
+    }
+    buttonSuccess<Emoji> {
+        id("someId")
+        emoji(Emoji.from("test"))
+    }
+    buttonDanger<Label> {
+        id("someId")
+        label("My Label")
+    }
+    buttonDanger<Emoji> {
+        id("someId")
+        emoji(Emoji.from("test"))
+    }
+    buttonLink<Label> {
+        id("someId")
+        label("My Label")
+        url("https://someurl.de")
+    }
+    buttonLink<Emoji> {
+        id("someId")
+        emoji(Emoji.from("test"))
+        url("https://someurl.de")
+    }
+    menu {
+        id("someId")
+        option {
+            label("My Label")
+            value("Some Value")
+        }
+        option {
+            label("My Label")
+            value("Some Value")
+            description = "My Description"
+            default = true
+            emoji = Emoji.from("test")
+        }
+    }
+}*/
