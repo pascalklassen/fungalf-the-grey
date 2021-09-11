90d97270347927c3aa0f78117c9ab54b3d4e1d10
Pascal Klaßen <p.klassen221199@gmail.com>
2021-09-11 14:59:21 +0200

    Test exposed framework

--- src/main/kotlin/io/github/pascalklassen/fungalf/extensions/PokeCordExtension.kt
+++ src/main/kotlin/io/github/pascalklassen/fungalf/extensions/PokeCordExtension.kt
@@ -4,9 +4,16 @@ import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.converters.impl.string
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.chatGroupCommand
-import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.select
+import com.kotlindiscord.kord.extensions.utils.respond
+import dev.kord.rest.builder.message.create.embed
+import io.vertx.kotlin.coroutines.await
+import mu.KotlinLogging
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.lang.Exception
+import io.github.pascalklassen.pokefuture.pokemon.Pokemon as PokemonData
+
+private val LOGGER = KotlinLogging.logger {}
 
 class PokeCordExtension: Extension() {
     override val name = "pokecord"
@@ -15,6 +22,11 @@ class PokeCordExtension: Extension() {
     private val validStarter = listOf("bisasam", "glumanda", "shiggy")
 
     override suspend fun setup() {
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.create(Pokemon)
+        }
+
         chatGroupCommand {
             name = "extensions.pokecord.commandName"
             aliasKey = "extensions.pokecord.commandAliases"
@@ -25,10 +37,49 @@ class PokeCordExtension: Extension() {
                 description = "extensions.pokecord.claim.commandDescription"
 
                 action {
-                    val name = arguments.name.lowercase()
+                    val pokemonName = arguments.name.lowercase()
 
-                    transaction {
-                        val pokemon = Pokemon.select { Pokemon.name eq name }.first()
+                    var result = transaction { Pokemon.select { Pokemon.name eq pokemonName }.singleOrNull() }
+
+                    if (result == null) {
+                        LOGGER.debug { "Could not find Pokémon '$pokemonName' in Database!" }
+
+                        try {
+                            val data = PokemonData.fetch(pokemonName).await()
+
+                            result = transaction {
+                                val id = Pokemon.insert {
+                                    it[id] = data.id
+                                    it[name] = data.name
+                                    it[baseExperience] = data.baseExperience
+                                    it[weight] = data.weight
+                                    it[height] = data.height
+                                    it[maleSprite] = data.sprites.frontDefault
+                                    it[femaleSprite] = data.sprites.frontFemale
+                                    it[artwork] = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png"
+                                } get Pokemon.id
+
+                                Pokemon.select { Pokemon.id eq id }.single()
+                            }
+                        } catch (ex: Exception) {
+                            LOGGER.error { "Something went wrong while retrieving Pokémon Data for '$pokemonName'." }
+                        }
+                    }
+
+                    result?.let {
+                        message.respond {
+                            embed {
+                                title = "${it[Pokemon.name]} #${it[Pokemon.id]}"
+                                image = it[Pokemon.artwork]
+
+                                description = """
+                                    Gewicht: ${it[Pokemon.weight] / 10}kg
+                                    Höhe:    ${it[Pokemon.height] / 10}m
+                                    
+                                    Basis-Erfahrung: ${it[Pokemon.baseExperience]}-XP
+                                """.trimIndent()
+                            }
+                        }
                     }
                 }
             }
@@ -49,8 +100,8 @@ object Pokemon: Table() {
     val baseExperience = integer("base_experience")
     val weight = integer("weight")
     val height = integer("height")
-    val maleSprite = varchar("male_sprite", 255)
-    val femaleSprite = varchar("female_sprite", 255)
+    val maleSprite = varchar("male_sprite", 255).nullable()
+    val femaleSprite = varchar("female_sprite", 255).nullable()
     val artwork = varchar("artwork", 255)
 
     override val primaryKey = PrimaryKey(id)
